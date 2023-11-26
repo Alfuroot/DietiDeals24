01574a5df914208308138b89ae3a1b1adc92ec97
Alfuroot <giuseppe.carannante4@studenti.unina.it>
2023-11-26 18:08:20 +0100

    Initial Commit

---
+++ DietiDeals24/DietiDeals24App.swift
@@ -0,0 +1,17 @@
+//
+//  DietiDeals24App.swift
+//  DietiDeals24
+//
+//  Created by Giuseppe Carannante on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DietiDeals24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
