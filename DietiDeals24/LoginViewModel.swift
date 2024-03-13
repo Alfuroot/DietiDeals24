8868854cf953b6afbb60df7dacd24d3d8d4d394c
alessadegregorio <alessa.degregorio@studenti.unina.it>
2024-03-13 22:52:06 +0100

    usermodel added & func createUser

--- DietiDeals24/LoginViewModel.swift
+++ DietiDeals24/LoginViewModel.swift
@@ -15,6 +15,7 @@ class LoginViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var passwordConfirm: String = ""
     @Published var showRegistrationErrorAlert: Bool = false
+    var tmpuser: User?
     
     var isLoginDisabled: Bool {
             return username.isEmpty || password.isEmpty
@@ -32,5 +33,11 @@ class LoginViewModel: ObservableObject {
     func toggleVendor() {
         isBuyer = false
         isVendor.toggle()
+    }
+    
+    func creatUser() {
+        tmpuser?.username = username
+        tmpuser?.email = email
+        tmpuser?.password = password
     }
 }
